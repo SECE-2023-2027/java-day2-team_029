1e6ad0e2669d3fd36060fcebc63d1f6fc8c78139
Abii06 <abiramiarasu626@gmail.com>
2026-06-16 22:57:26 +0530

    Program 2 Completed

---
+++ Program_2.java
@@ -0,0 +1,32 @@
+public class Program_2 {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+        int sum = 0;
+
+        while (count < 100) {
+            if (isPrime(number)) {
+                sum += number;
+                count++;
+            }
+            number++;
+        }
+
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
